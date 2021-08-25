18211f3b4f287a3399990ccb8c92a85a81145675
Lama Fakhreddine <77051181+LamaFakhreddine@users.noreply.github.com>
2021-08-25 14:41:52 +0300

    Completed Sidebar
    - added hover effects
    - fixed nav-link alignments

--- edd-project/src/components/Sidebar.js
+++ edd-project/src/components/Sidebar.js
@@ -9,15 +9,15 @@ import "./Sidebar.scss";
 export default function NavbarSection() {
     return (
         <div className="sidebar">
-            <Navbar expand="true" variant="dark">
-                <Container className="flex-column">
+            <Navbar expand="true">
+                <Container className="d-flex justify-content-center">
                     <Navbar.Brand href="#home">
                         <img src={LogoImg} alt="Edd Logo" className="navbar-logo"/>
                     </Navbar.Brand>
-                    <Nav>
-                        <Nav.Link href="#home"><i class="fas fa-home"></i>Home</Nav.Link>
-                        <Nav.Link href="#calendar"><i class="far fa-calendar"></i>Calendar</Nav.Link>
-                        <Nav.Link href="#pricing"><i class="fas fa-tasks"></i>Tasks</Nav.Link>
+                    <Nav className="">
+                        <Nav.Link href="#home" className="d-flex flex-column"><i class="fas fa-home fa-lg"></i>Home</Nav.Link>
+                        <Nav.Link href="#calendar" className="d-flex flex-column"><i class="far fa-calendar fa-lg"></i>Calendar</Nav.Link>
+                        <Nav.Link href="#pricing" className="d-flex flex-column"><i class="fas fa-tasks fa-lg"></i>Tasks</Nav.Link>
                     </Nav>
                 </Container>
             </Navbar>
