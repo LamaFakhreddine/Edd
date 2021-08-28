6367eca61d1920533016e8e8fa6f6500b386c01e
Lama Fakhreddine <77051181+LamaFakhreddine@users.noreply.github.com>
2021-08-28 11:55:30 +0300

    ProgressBar.js completed
    - completed styling
    - created  links to weekdays
    - created more scss variables

--- edd-project/src/components/ProgressBar.js
+++ edd-project/src/components/ProgressBar.js
@@ -1,19 +1,24 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import "./ProgressBar.scss";
 
 function ProgressBar() {
     const WEEKDAYS = ['S','M','T','W','T','F','S'];
     return (
-        <div className="steps-bar d-flex flex-row">
-            {WEEKDAYS.map((item, index) => {
-                return ( 
-                <div className="step">
-                    <div className="step-bubble"></div>
-                    <div key={index} className="step-label">{item}</div>
-                </div>
-                );
-            
-            })}
+        <div className="progress-bar">
+            <h2>Week Planner</h2>
+            <div className="steps-bar d-flex flex-row">
+                {WEEKDAYS.map((weekday, index) => {
+                    return ( 
+                        // add completed class only if the day is already completed
+                    <Link to="/" key={index+weekday} className={`step ${index < new Date().getDay()? "completed" : ""}`}>
+                        <div className="step-bubble"></div>
+                        <div className="step-label">{weekday}</div>
+                    </Link>
+                    );
+                
+                })}
+            </div>
         </div>
     )
 }
