1ec432f1b9aec532cc6eb9899af234750a062e0c
Lama Fakhreddine <77051181+LamaFakhreddine@users.noreply.github.com>
2021-08-31 08:32:36 +0300

    TaskItem.js
    - added delete  btn (yet to add delete funct)
    - made text editable

--- edd-project/src/components/TaskItem.js
+++ edd-project/src/components/TaskItem.js
@@ -9,12 +9,19 @@ function TaskItem() {
         setCompleted(!completed);
     }
 
+    const handleDelete = (event) => {
+
+    };
+
     return (
         <div className="task-item d-flex align-items-center" style={completed? {filter: "opacity(0.5)"} : {backgroundColor: "#fff4f4"}}>
             <div className="task-time">9:05</div>
             <div className="task-time-am-pm">AM</div>
-            <div className="task-name">Studying</div>
-            <div className="ms-auto checkbox-wrapper">
+            <div contentEditable="true" className="task-name">Studying</div>
+            <div className="ms-auto d-flex align-items-center wrapper">
+                <div className="delete" onClick={handleDelete}>
+                    <i className="far fa-trash-alt"></i>
+                </div>
                 <div className="checkbox" onClick={handleClick}>
                     {completed? <i class="fas fa-check"></i> : <></>}  
                 </div>         
